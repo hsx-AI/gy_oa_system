8c1210f8ecce22bfda4cce4aa12fd8df16da4101
hsx <15233552792@163.com>
2026-06-04 14:54:22 +0800

    修改

--- vue-project/src/views/InfoFeed.vue
+++ vue-project/src/views/InfoFeed.vue
@@ -175,19 +175,13 @@ const cityOptions = [
 ]
 
 const newsTypes = [
-  { label: '头条', value: 'top' },
-  { label: '国内', value: 'guonei' },
   { label: '国际', value: 'guoji' },
-  { label: '财经', value: 'caijing' },
-  { label: '娱乐', value: 'yule' },
-  { label: '体育', value: 'tiyu' },
   { label: '军事', value: 'junshi' },
   { label: '科技', value: 'keji' },
-  { label: '社会', value: 'shehui' },
 ]
 
 const selectedLocation = ref('101050101')
-const selectedNewsType = ref('top')
+const selectedNewsType = ref('guoji')
 const summary = ref({})
 const weatherNow = ref(null)
 const hourlyForecast = ref(null)
